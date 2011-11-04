cf138a21c4044d3c8ef22349a2021ec14b7ea10c
Tyler Hunt <tyler@tylerhunt.com>
2011-11-04 13:05:47 -0700

    Stub #execute instead of the internal #send_gem.

--- lib/rubygems/commands/fake_push_command.rb
+++ lib/rubygems/commands/fake_push_command.rb
@@ -3,7 +3,8 @@ require 'rubygems/commands/push_command'
 # Because of the way Gem commands are loaded, it isn't possible at this time to
 # override the #send_gem method without resorting to monkey patching.
 class Gem::Commands::PushCommand < Gem::Command
-  def send_gem(name)
+  def execute
+    name = get_one_gem_name
     gem_name, version = File.basename(name).scan(/(.*)-([\d\.]+).gem/).first
 
     sleep 0.5
